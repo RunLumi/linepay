323dca0dc1dd7054cdb03820b02621d698033aa1
Stream Entry <978862+streamentry@users.noreply.github.com>
2026-09-05 11:32:35 +0700

    style(ios): make design tokens formatter-clean

--- apps/ios/App/Sources/DesignTokens.swift
+++ apps/ios/App/Sources/DesignTokens.swift
@@ -19,8 +19,8 @@ enum LinePayColor {
     }
 }
 
-private extension UIColor {
-    convenience init(rgb: UInt32) {
+extension UIColor {
+    fileprivate convenience init(rgb: UInt32) {
         self.init(
             red: CGFloat((rgb >> 16) & 0xFF) / 255,
             green: CGFloat((rgb >> 8) & 0xFF) / 255,
