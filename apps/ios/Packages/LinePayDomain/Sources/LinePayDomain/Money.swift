50cdcbbfadfe543ed448e83c8c166c0aa63f4145
Stream Entry <978862+streamentry@users.noreply.github.com>
2026-09-05 11:02:24 +0700

    feat(domain): make money arithmetic explicit and rounded

--- apps/ios/Packages/LinePayDomain/Sources/LinePayDomain/Money.swift
+++ apps/ios/Packages/LinePayDomain/Sources/LinePayDomain/Money.swift
@@ -9,12 +9,65 @@ public struct Money: Codable, Hashable, Sendable {
         self.currencyCode = currencyCode.uppercased()
     }
 
+    public static func zero(currencyCode: String) -> Money {
+        Money(amount: 0, currencyCode: currencyCode)
+    }
+
     public func adding(_ other: Money) throws -> Money {
+        try requireSameCurrency(as: other)
+        return Money(amount: amount + other.amount, currencyCode: currencyCode)
+    }
+
+    public func subtracting(_ other: Money) throws -> Money {
+        try requireSameCurrency(as: other)
+        return Money(amount: amount - other.amount, currencyCode: currencyCode)
+    }
+
+    public func multiplied(by multiplier: Decimal) -> Money {
+        Money(amount: amount * multiplier, currencyCode: currencyCode)
+    }
+
+    public func rounded(using rule: MoneyRoundingRule) -> Money {
+        var input = amount
+        var output = Decimal()
+        NSDecimalRound(&output, &input, rule.scale, rule.mode.foundationMode)
+        return Money(amount: output, currencyCode: currencyCode)
+    }
+
+    private func requireSameCurrency(as other: Money) throws {
         guard currencyCode == other.currencyCode else {
             throw MoneyError.currencyMismatch(lhs: currencyCode, rhs: other.currencyCode)
         }
+    }
+}
 
-        return Money(amount: amount + other.amount, currencyCode: currencyCode)
+public struct MoneyRoundingRule: Codable, Hashable, Sendable {
+    public let scale: Int
+    public let mode: MoneyRoundingMode
+
+    public init(scale: Int = 2, mode: MoneyRoundingMode = .halfUp) {
+        self.scale = scale
+        self.mode = mode
+    }
+}
+
+public enum MoneyRoundingMode: String, Codable, Hashable, Sendable {
+    case halfUp
+    case bankers
+    case down
+    case up
+
+    fileprivate var foundationMode: Decimal.RoundingMode {
+        switch self {
+        case .halfUp:
+            .plain
+        case .bankers:
+            .bankers
+        case .down:
+            .down
+        case .up:
+            .up
+        }
     }
 }
 
